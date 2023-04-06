8c57fd3073041de3141889f589123868572f1e59
druid3122 <asta199301@outlook.com>
2023-04-06 15:33:26 +0800

    init commit

---
+++ src/main/java/com/project/nulinknft/NulinkNftApplication.java
@@ -0,0 +1,13 @@
+package com.project.nulinknft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NulinkNftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NulinkNftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/nulinknft/NulinkNftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.nulinknft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NulinkNftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
